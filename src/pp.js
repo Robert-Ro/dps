705fa84d6c019e62b9cf2cb495a8d883da438c3f
liutsing <liutsingluo@gmail.com>
2022-06-28 10:32:45 +0800

    chore: ~

--- src/pp.js
+++ src/pp.js
@@ -7,16 +7,18 @@ const devices = {
   pc: [1200, 1000, 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1']
 };
 
-async function pp({device = 'mobile', headless = true}) {
+async function pp({device = 'mobile', headless = true, cookies= []}) {
   const browser = await ppteer.launch({headless});
-  
+
   async function openPage(url, extraHTTPHeaders) {
     const page = await browser.newPage();
     try {
       let deviceSet = devices[device];
       page.setUserAgent(deviceSet[2]);
       page.setViewport({width: deviceSet[0], height: deviceSet[1]});
-
+      if(cookies.length){
+        await page.setCookie(...cookies)
+      }
       if(extraHTTPHeaders && getAgrType(extraHTTPHeaders) === 'object') {
         await page.setExtraHTTPHeaders(new Map(Object.entries(extraHTTPHeaders)));
       }
